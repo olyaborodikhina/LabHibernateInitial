422f71d0b9cefa0500d0a6613d467aab698aab42
olyaborodikhina <olyaborodikhina@yandex.ru>
2016-11-28 15:53:29 +0700

    All the Applications wiht Hibernate

---
+++ lab-04-entity-hierarchy/src/main/java/edu/jpa/TABLE_PER_CLASS/entity/Person.java
@@ -0,0 +1,32 @@
+package edu.jpa.TABLE_PER_CLASS.entity;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+/**
+ * @author Anton German &lt;AGerman@luxoft.com&gt;
+ * @version 1.0 15.02.15
+ */
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Person {
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
